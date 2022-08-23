d7c1b45b9e6c3c2652432bd03cc89b28e055208e
Hanna Marcus <hannamarcus@Hannas-MacBook-Air.local>
2022-08-22 21:44:29 -0700

    all files working

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,35 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}  
+  [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+  ## Description:
+  ${data.description}
+  ## Table of Contents:
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Testing](#testing)
+  - [Questions](#questions)
 
-`;
+  ## Installation:  
+  ${data.installation}
+  
+  ## Usage:
+  ${data.usage}
+  
+  ## License:
+  ${data.license}
+
+  ## Contributing:
+  ${data.contibution}
+
+  ## Testing:
+  In order to test run ${data.test} in the GitBash terminal
+
+  ## Questions:
+  Any questions? Contact me on [Github](https://github.com/${data.username})
+  or by email at ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
